3230c546eab9476abc5c02409ec3974c9cec3499
BE\vspruyt <vspruyt@deloitte.com>
2021-04-15 15:52:29 +0200

    Testing branching

---
+++ force-app/main/default/lwc/accountDeliveryPointInfoCard/someFile.js
@@ -0,0 +1,19 @@
+// #region Imports
+// import { ShowToastEvent } from "lightning/platformShowToastEvent";
+// #endregion
+
+// #region Constants
+const MY_CONSTANT = "Name";
+// #endregion
+
+// #region Classes & functions
+// class MyClass {...}
+// function myFunction(){...}
+// const myArrowFunction = () => {...}
+// #endregion
+
+// #region Exports
+export {
+    MY_CONSTANT as myAliasedConstant
+};
+// #endregion
